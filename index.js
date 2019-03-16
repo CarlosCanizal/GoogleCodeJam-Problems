51029f4e61a5123d906a1501ffefcb117be440f8
CarlosCanizal <carlos@canizal.com>
2019-03-16 15:18:24 -0600

    Process line and print case

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const readline = require('readline');
-const local = true;
+const LOCAL_PRACTICE = true;
 let rl;
-if(local){
+if(LOCAL_PRACTICE){
     const fs = require('fs');
     rl = readline.createInterface({
         input: fs.createReadStream('input.txt')
@@ -10,9 +10,15 @@ if(local){
     rl = readline.createInterface(process.stdin, process.stdout);
 }
 
-
-var lineno = 0;
-rl.on('line', function (line) {
-  lineno++;
-  console.log('Line number ' + lineno + ': ' + line);
+var lineNumber = 0;
+rl.on('line', line=>{
+  lineNumber++;
+  let lineResult = processLine(lineNumber,line);
+  console.log(`Case #${lineNumber}: ${lineResult}`);
+}).on('close',()=>{
+    process.exit(0);
 });
+
+const processLine = (lineNumber, line)=>{
+    return line;
+}
